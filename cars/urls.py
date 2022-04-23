2331c811cea660a08128dc2fe3863e43e04f67b1
Kamaljeet <kamaljeetsahoo@gmail.com>
2022-04-24 00:35:41 +0530

    Car Model Serializer done

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import test_view
+from .views import test_view, CarApiOverview
 
 urlpatterns = [
     path('hello/', test_view),
+    path('overview/', CarApiOverview)
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Car
+from .serlializers import CarSerializer
 
 # Create your views here.
 def test_view(request):
     return HttpResponse("<h1>Hello</h1>")
+
+@api_view(['GET'])
+def CarApiOverview(request):
+    api_urls = {
+        'all_items' : '/'
+    }
+    return Response(api_urls)
